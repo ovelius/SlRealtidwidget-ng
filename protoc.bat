2f01b498c2bcecf27db5239bb6ad793da20403c6
ovelius <erik.ovelius@gmail.com>
2023-11-05 17:47:13 +0100

    Protoc update

--- protoc.bat
+++ protoc.bat
@@ -1,3 +1,5 @@
 rem Requires protoc and protoc-gen-javalite
 rem https://search.maven.org/search?q=g:com.google.protobuf
-protoc.exe messages.proto --javalite_out=app/src/main/java
+protoc.exe messages.proto --java_out=lite:app/src/main/java
+protoc.exe messages.proto --java_out=../Sl-Realtime-project/PublicTransportBe/src/main/java
+protoc.exe messages.proto --kotlin_out=../Sl-Realtime-project/PublicTransportBe/src/main/kotlin
